90550a2b2ed3dffe9da005c6b88f08cd05c64521
Chomentowski Sebastian <chomentowski.s@gmail.com>
2021-11-15 19:15:48 +0100

    first floor string, function to read board string

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ PLAYER_ICON = '@'
 PLAYER_START_X = 3
 PLAYER_START_Y = 3
 
-BOARD_WIDTH = 30
-BOARD_HEIGHT = 20
+BOARD_WIDTH = 20
+BOARD_HEIGHT = 30
 
+board = []
 
+for i in range(BOARD_WIDTH):
+    board.append("# ")
+print(board)
 def create_player():
     '''
     Creates a 'player' dictionary for storing all player related informations - i.e. player icon, player position.
